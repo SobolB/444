b350ca26a860aa1095268d9c50ac6868e140ac31
SobolB <79850869+SobolB@users.noreply.github.com>
2023-01-10 19:54:52 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         BonusMilesService service = new BonusMilesService();
-        int cost = service.calculate(true, 1100, 100, 1000);
-        System.out.println(cost);
+        int price = 10_000;
+        int miles = service.calculate(price);
+        System.out.println(miles);
     }
 }
