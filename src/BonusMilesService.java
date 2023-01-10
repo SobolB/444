e9e6e0865a1fcb137921553216136762a3377868
SobolB <79850869+SobolB@users.noreply.github.com>
2023-01-10 19:52:03 +0300

    Update BonusMilesService.java

--- src/BonusMilesService.java
+++ src/BonusMilesService.java
@@ -1,15 +1,5 @@
 public class BonusMilesService {
-    public int calculate(boolean counts, int count, int summ1, int min) {
-        int summ2 = summ1 + count;
-        int bonus = count / 100;
-        int percent = counts ? bonus : summ2;
-        int mill1 = percent + summ2;
-        if (summ2 > min) {
-        }
-        else
-        {
-            mill1 = summ2;
-        }
-        return mill1;
+    public int calculate(int cost) {
+        return cost / 20;
     }
 }
